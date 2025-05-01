10d5f104e59fe3bd284cf42605ae018a07a30684
Ece Yaman <eceymn35@gmail.com>
2025-05-01 15:39:36 +0300

    initial commit

---
+++ src/main/java/com/workintech/twitter_api_challenge/TwitterApiChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.workintech.twitter_api_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterApiChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterApiChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workintech/twitter_api_challenge/TwitterApiChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.twitter_api_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterApiChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
